9927b236158a085a3efcd400c2274bb730ba1050
guidogerb <garygerber@bluepantsmedia.com>
2021-09-13 18:11:03 -0600

    fixed apply errors

--- main.tf
+++ main.tf
@@ -2,19 +2,101 @@ provider "aws" {
   region                  = local.region
   shared_credentials_file = "~/.aws/credentials"
   profile                 = "garygerber"
-  default_tags {
-    tags = {
-      Owner = "Bluepants Media, LLC"
-      CreatedBy = "Gary Gerber"
-      GeneratedBy = "Terriform"
-    }
-  }
 }
 
 locals {
   prepend-name   = "BPM-"
   region = "us-east-2"
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "${local.prepend-name}VPC"
+  cidr = "10.1.0.0/16"
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
+  private_subnets  = ["10.1.3.0/24", "10.1.4.0/24", "10.1.5.0/24"]
+  database_subnets = ["10.1.6.0/24", "10.1.7.0/24", "10.1.8.0/24"]
+
+  enable_ipv6 = false
+
+  enable_nat_gateway = false
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "${local.prepend-name}public-subnet"
+  }
+
+  private_subnet_tags = {
+    Name = "${local.prepend-name}private-subnet"
+  }
+
+  database_subnet_tags = {
+    Name = "${local.prepend-name}database-subnet"
+  }
+
+  private_route_table_tags = {
+    Name = "${local.prepend-name}route-table-private"
+  }
+
+  public_route_table_tags = {
+    Name = "${local.prepend-name}route-table-public"
+  }
+
+  nat_gateway_tags = {
+    Name = "${local.prepend-name}nat-gateway"
+  }
+
+  vpc_flow_log_tags = {
+    Name = "${local.prepend-name}flow-logs"
+  }
+
+  default_security_group_tags = {
+    Name = "${local.prepend-name}default-security-group"
+  }
+
+  igw_tags = {
+    Name = "${local.prepend-name}internet-gateway"
+  }
+
+  default_route_table_tags = {
+    Name = "${local.prepend-name}default-route-table"
+    DefaultRouteTable = true
+  }
+
+  tags = {
+    Owner = "707871397046"
+    CreatedBy = "Gary Gerber"
+    GeneratedBy = "Terriform"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "${local.prepend-name}VPC"
+  }
+
+  create_database_subnet_group = false
+
+  manage_default_route_table = true
+
+  # Default security group - ingress/egress rules cleared to deny all
+  manage_default_security_group  = true
+  default_security_group_ingress = []
+  default_security_group_egress  = []
+
+  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
+
+}
+
+/*
+
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${module.vpc.vpc_id}"
@@ -164,20 +246,7 @@ resource "aws_route_table_association" "private_c" {
   route_table_id = "${aws_route_table.private.id}"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = "${local.prepend-name}VPC"
-  cidr = "10.1.0.0/16"
-
-  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets   = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
-  private_subnets  = ["10.1.3.0/24", "10.1.4.0/24", "10.1.5.0/24"]
-  database_subnets = ["10.1.6.0/24", "10.1.7.0/24", "10.1.8.0/24"]
-
-}
-
+*/
 
 ################################################################################
 # RDS Aurora Module - PostgreSQL
